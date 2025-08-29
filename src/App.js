67d8907a0cbc385cc39472190a9d460752243fef
Aleksei <tayrenov@gmail.com>
2025-08-29 10:51:42 +0300

    upd dark-light animation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import './App.css';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchTodos, checkDbStatus } from './app/redux/todosActions.js';
-import { switchTheme } from './app/redux/uiActions.js';
+import './App.css';
 
 import LeftPanel from './components/left-panel/left-panel.jsx';
 import Header from './components/header/header.jsx';
+import MainPanel from './components/main-panel/main-panel.jsx';
 
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import IndexPage from './pages/index_page.jsx';
-import TodosListPage from './pages/todos/todosList_page.jsx';
-import TodoItemPage from './pages/todos/todoItem_page.jsx';
-import { testAuthTrue } from './app/redux/appActions.js';
 
 function App() {
-  const dispatch = useDispatch();
-
-  const { todos, loading, error, dbStatus, currentSource } = useSelector(state => state.todos);
   const { mainTheme } = useSelector(state => state.ui);
-  const { auth } = useSelector(state => state.app);
-
-  const handleLoad = () => dispatch(fetchTodos())
-
-  const checkStatus= () => dispatch(checkDbStatus())
-
-  const hundleSwitchTheme = () => dispatch(switchTheme())
-
-  const hundleAuth = () => dispatch(testAuthTrue())
-
+  
   return (
     <Router>
 
@@ -43,31 +26,7 @@ function App() {
             <Route path='/todo/:id' element={<Header title='Todos-page-id'/>} />
           </Routes>
 
-          <div className='app-main-panel'>
-
-            <Routes>
-              <Route path='/' element={<IndexPage/>}/>
-              <Route path='/todos' element={<TodosListPage />}/>
-              <Route path='/todo/:id' element={<TodoItemPage />} />
-            </Routes>
-
-            <button onClick={hundleAuth}>Set Auth: {auth ? '1' : '0'}</button>
-
-            <button onClick={hundleSwitchTheme}>{console.log(mainTheme)}TEST THEME</button>
-
-            <button onClick={checkStatus} disabled={loading}>
-              Check Status
-            </button>
-            <p>Status: {dbStatus}</p>
-            <button onClick={handleLoad} disabled={loading}>
-              {loading ? "Loading..." : 'Load Todos'}
-            </button>
-
-            {error && <p style={{color:'red'}}>Error: { error }</p>}
-
-            <pre>{JSON.stringify(todos.slice(0,3), null, 2)}</pre>
-
-          </div>
+          <MainPanel />
 
         </div>
 
--- src/components/main-panel/main-panel.jsx
+++ src/components/main-panel/main-panel.jsx
@@ -1,18 +1,52 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import './main-panel.css'
+
+import IndexPage from '../../pages/index_page.jsx';
+import TodosListPage from '../../pages/todos/todosList_page.jsx';
+import TodoItemPage from '../../pages/todos/todoItem_page.jsx';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTodos, checkDbStatus } from '../../app/redux/todosActions.js';
+import { switchTheme } from '../../app/redux/uiActions.js';
+import { testAuthTrue } from '../../app/redux/appActions.js';
+
 function MainPanel() {
+
+    const dispatch = useDispatch();
+
+    const { todos, loading, error, dbStatus, currentSource } = useSelector(state => state.todos);
+    const { mainTheme } = useSelector(state => state.ui);
+    const { auth } = useSelector(state => state.app);
+
+    const checkStatus= () => dispatch(checkDbStatus())
+
+    const hundleAuth = () => dispatch(testAuthTrue())
+
+    const handleLoad = () => dispatch(fetchTodos())
+
     return (
         <div className="app-main-panel">
 
-          <button onClick={checkStatus} disabled={loading}>
-            Check Status
-          </button>
-          <p>Status: {dbStatus}</p>
-          <button onClick={handleLoad} disabled={loading}>
-            {loading ? "Loading..." : 'Load Todos'}
-          </button>
+            <Routes>
+              <Route path='/' element={<IndexPage/>}/>
+              <Route path='/todos' element={<TodosListPage />}/>
+              <Route path='/todo/:id' element={<TodoItemPage />} />
+            </Routes>
 
-          {error && <p style={{color:'red'}}>Error: { error }</p>}
+            <button onClick={hundleAuth}>Set Auth: {auth ? '1' : '0'}</button>
 
-          <pre>{JSON.stringify(todos.slice(0,3), null, 2)}</pre>
+            <button onClick={checkStatus} disabled={loading}>
+                Check Status
+            </button>
+            <p>Status: {dbStatus}</p>
+            <button onClick={handleLoad} disabled={loading}>
+                {loading ? "Loading..." : 'Load Todos'}
+            </button>
+
+            {error && <p style={{color:'red'}}>Error: { error }</p>}
+
+            <pre>{JSON.stringify(todos.slice(0,3), null, 2)}</pre>
 
         </div>
     )
